46b309e199d3a4557f55d3acb35a5f3af6e7e953
박재형 <48474102+Madlar@users.noreply.github.com>
2021-11-03 17:41:18 +0900

    axios 설치

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function Landingpage() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(res => console.log(res.data))
+    }, [])
+
     return (
         <div>
-            Landingpage
+            Landingpage 랜딩
         </div>
     )
 }
